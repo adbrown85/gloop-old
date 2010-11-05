4f4b476e429da7ae3e89221aa0bd185ca7cdd10d
Andrew Brown <adb1413@rit.edu>
2010-11-04 21:47:12 -0400

    testFlush and testDraw in VertexBufferObject test

--- test/geometry/VertexBufferObject.cxx
+++ test/geometry/VertexBufferObject.cxx
@@ -20,6 +20,8 @@ public:
 	void tearDown();
 	void testAllocate();
 	void testPut();
+	void testFlush();
+	void testDraw();
 private:
 	Window *window;
 	Canvas *canvas;
@@ -93,6 +95,66 @@ void VertexBufferObjectTest::testPut() {
 	assert(false);
 }
 
+/** Ensures the data can be sent to the card. */
+void VertexBufferObjectTest::testFlush() {
+	
+	VertexBufferObject *vbo;
+	
+	cout << "VertexBufferObjectTest::testFlush" << endl;
+	
+	vbo = new VertexBufferObject();
+	vbo->bind();
+	vbo->addAttribute("MCVertex", 3);
+	vbo->allocate(GL_STATIC_DRAW, 3);
+	
+	vbo->put(-0.5, +0.5, 0);
+	vbo->put(-0.5, -0.5, 0);
+	vbo->put(+0.5, +0.5, 0);
+	
+	vbo->flush();
+	ErrorChecker::assertNoError("testFlush");
+	cout << "PASSED" << endl;
+}
+
+class TestDrawListener : public CanvasListener {
+public:
+	virtual void onCanvasInitEvent(Canvas &canvas) {}
+	virtual void onCanvasDisplayEvent(Canvas &canvas);
+	virtual void onCanvasKeyEvent(Canvas &canvas) {}
+	virtual void onCanvasButtonEvent(Canvas &canvas) {}
+	virtual void onCanvasDragEvent(Canvas &canvas) {}
+};
+
+void TestDrawListener::onCanvasDisplayEvent(Canvas &canvas) {
+	
+//	glViewport(0, 512, 0, 512);
+	glClearColor(0, 1, 0, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void VertexBufferObjectTest::testDraw() {
+
+	VertexBufferObject *vbo;
+	
+	cout << "VertexBufferObjectTest::testDraw" << endl;
+	
+	vbo = new VertexBufferObject();
+	vbo->bind();
+	vbo->addAttribute("MCVertex", 3);
+	vbo->allocate(GL_STATIC_DRAW, 3);
+	
+	vbo->put(-0.5, +0.5, 0);
+	vbo->put(-0.5, -0.5, 0);
+	vbo->put(+0.5, +0.5, 0);
+	
+	vbo->flush();
+	
+	canvas->addListener(new TestDrawListener());
+	window->run();
+	
+	cout << "PASSED" << endl;
+}
+
 /* Runs the test. */
 int main(int argc, char *argv[]) {
 	
@@ -103,6 +165,8 @@ int main(int argc, char *argv[]) {
 	try {
 		test.testAllocate();
 		test.testPut();
+		test.testFlush();
+		test.testDraw();
 	} catch (exception &e) {
 		cerr << e.what() << endl;
 		cout << "FAILED!" << endl;
