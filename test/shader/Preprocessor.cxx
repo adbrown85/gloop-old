dc29a1f12cb6882efb4742a88e11f840d2614353
Andrew Brown <adb1413@rit.edu>
2010-11-05 20:40:47 -0400

    Preprocessor test asserts each line

--- test/shader/Preprocessor.cxx
+++ test/shader/Preprocessor.cxx
@@ -4,59 +4,52 @@
  * Author
  *     Andrew Brown <adb1413@rit.edu>
  */
+#include "gloop_common.h"
+#include <cassert>
 #include "Preprocessor.hpp"
 
-
+/** Unit test for Preprocessor. */
 class PreprocessorTest {
 public:
-	void setUp();
-	void testLines();
-private:
-	Preprocessor pp;
+	void testParse();
 };
 
-
-void PreprocessorTest::setUp() {
+/** Test a file can be parsed correctly. */
+void PreprocessorTest::testParse() {
 	
-	pp.parse("Preprocessor.glsl");
-}
-
-
-void PreprocessorTest::testLines() {
-	
+	Preprocessor pp;
 	Code code;
 	Code::iterator it;
+	int i;
+	string filename = "test/shader/basic.frag";
+	int numbers[6] = {1, 4, 7, 11, 13, 14};
+	string lines[6] = {"#version 130\n",
+	                   "uniform vec4 Color = vec4(1.0);\n",
+	                   "out vec4 FragColor;\n",
+	                   "void main() {\n",
+	                   "    FragColor = Color;\n",
+	                   "}\n"};
 	
+	cout << "PreprocessorTest::testParse" << endl;
+	
+	pp.parse(filename);
 	code = pp.getCode();
-	for (it=code.begin(); it!=code.end(); ++it)
-		cout << it->filename << " " << it->number << " " << it->text;
+	for (it=code.begin(); it!=code.end(); ++it) {
+		i = distance(code.begin(), it);
+		assert(it->filename == filename);
+		assert(it->number == numbers[i]);
+		assert(it->text == lines[i]);
+	}
+	
+	cout << "PASSED" << endl;
 }
-
 
 int main(int argc, char *argv[]) {
 	
 	PreprocessorTest test;
 	
-	// Start
-	cout << endl;
-	cout << "****************************************" << endl;
-	cout << "Preprocessor" << endl;
-	cout << "****************************************" << endl;
-	cout << endl;
-	
-	try {
-		test.setUp();
-		test.testLines();
-	} catch (exception &e) {
-		cerr << e.what() << endl;
-	}
-	
-	// Finish
-	cout << endl;
-	cout << "****************************************" << endl;
-	cout << "Preprocessor" << endl;
-	cout << "****************************************" << endl;
-	cout << endl;
+	test.testParse();
+	cout << "ALL TESTS PASSED" << endl;
 	return 0;
 }
 
