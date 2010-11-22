7dba01aeb24021d323c2133af2775f861c0abec5
Andrew Brown <adb1413@rit.edu>
2010-11-22 00:47:30 -0500

    DatasetHeader test fixed, uses framework

--- test/volume/DatasetHeader.cxx
+++ test/volume/DatasetHeader.cxx
@@ -4,47 +4,47 @@
  * Author
  *     Andrew Brown <adb1413@rit.edu>
  */
+#include "gloop_common.h"
+#include "../Test.h"
 #include "DatasetHeader.hpp"
+#define DATASETHEADERTEST_FILE "test/volume/bunny128.vlb"
 
+/** @brief Unit test for DatasetHeader. */
+class DatasetHeaderTest : public Test {
+public:
+	DatasetHeaderTest();
+	virtual ~DatasetHeaderTest();
+	void testConstructor();
+private:
+	DatasetHeader *header;
+};
 
-int main(int argc,
-         char *argv[]) {
-	
-	string filename;
-	
-	// Handle arguments
-	if (argc == 1)
-		filename = "../../textures/bunny128.vlb";
-	else if (argc == 2)
-		filename = argv[1];
-	else {
-		cerr << "Usage: " << argv[0] 
-		     << " [<filename>]" << endl;
-		exit(1);
-	}
-	
-	// Start
-	cout << endl;
-	cout << "****************************************" << endl;
-	cout << "DatasetHeader" << endl;
-	cout << "****************************************" << endl;
-	cout << endl;
-	
-	// Test
-	try {
-		DatasetHeader header(filename);
-		header.print();
-	}
-	catch (exception &e) {
-		cerr << e.what() << endl;
-	}
-	
-	// Finish
-	cout << endl;
-	cout << "****************************************" << endl;
-	cout << "DatasetHeader" << endl;
-	cout << "****************************************" << endl;
-	cout << endl;
-	return 0;
+/** Creates the header. */
+DatasetHeaderTest::DatasetHeaderTest() {
+	header = new DatasetHeader(DATASETHEADERTEST_FILE);
 }
+
+/** Destroys the header. */
+DatasetHeaderTest::~DatasetHeaderTest() {
+	delete header;
+}
+
+/** Makes sure all the values were read correctly. */
+void DatasetHeaderTest::testConstructor() {
+	assert(header->getWidth() == 128);
+	assert(header->getHeight() == 128);
+	assert(header->getDepth() == 90);
+	assert(header->getType() == "uint8");
+	assert(header->getMin() == 0);
+	assert(header->getMax() == 255);
+	assert(header->getLow() == 0);
+	assert(header->getHigh() == 255);
+}
+
+/* Run the test. */
+#define HARNESS DatasetHeaderTest
+#include "../Runner.h"
+START_TESTS
+ADD_TEST(testConstructor)
+RUN_TESTS
 
