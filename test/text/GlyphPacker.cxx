7a7eab6c6bdce12cd045e2c69f38b17da6373e5d
Andrew Brown <adb1413@rit.edu>
2010-12-13 22:59:59 -0500

    Cleaned up GlyphPacker test, added testLocations

--- test/text/GlyphPacker.cxx
+++ test/text/GlyphPacker.cxx
@@ -12,42 +12,59 @@
 /** Test fixture for GlyphPacker. */
 class GlyphPackerTest : public Test {
 public:
+	virtual void setUp();
+	virtual void tearDown();
 	void testMeasure();
 	void testPack();
+	void testLocations();
+private:
+	Font *font;
+	GlyphPacker *packer;
+	GlyphPackage package;
 };
+
+void GlyphPackerTest::setUp() {
+	
+	font = new Font("Arial", Font::PLAIN, 72);
+	packer = new GlyphPacker(*font);
+	packer->pack();
+	package = packer->getPackage();
+}
+
+void GlyphPackerTest::tearDown() {
+	
+	delete font;
+	delete packer;
+}
 
 /** Ensures font is measured properly. */
 void GlyphPackerTest::testMeasure() {
 	
-	Font font("Arial", Font::PLAIN, 72);
-	GlyphPacker packer(font);
-	GlyphPackage gp;
-	
-	packer.pack();
-	gp = packer.getPackage();
-	
-//	cout << gp.width << endl;
-//	cout << gp.height << endl;
-//	cout << gp.rows << endl;
-//	cout << gp.cols << endl;
-	
-	assert(gp.width == 748);
-	assert(gp.height == 747);
-	assert(gp.rows == 9);
-	assert(gp.cols == 11);
+	assert(package.width == 748);
+	assert(package.height == 747);
+	assert(package.rows == 9);
+	assert(package.cols == 11);
 }
 
 /** Ensures font is packed into the image properly. */
 void GlyphPackerTest::testPack() {
 	
-	Font font("Arial", Font::PLAIN, 72);
-	GlyphPacker packer(font);
-	GlyphPackage gp;
+	package.image->write_to_png("test/text/GlyphPacker.png");
+}
+
+void GlyphPackerTest::testLocations() {
 	
-	packer.pack();
-	gp = packer.getPackage();
+	map<char,GlyphLocation>::iterator it;
 	
-	gp.image->write_to_png("test/text/GlyphPacker.png");
+	it = package.locations.find('A');
+	if (it == package.locations.end()) {
+		throw BasicException("Could not find location!");
+	}
+	
+	assert(it->second.x == 680);
+	assert(it->second.y == 233);
+	assert(it->second.height == 83);
+	assert(it->second.width == 50);
 }
 
 /* Run the test. */
@@ -56,4 +73,5 @@ void GlyphPackerTest::testPack() {
 START_TESTS
 ADD_TEST(testMeasure)
 ADD_TEST(testPack)
+ADD_TEST(testLocations)
 RUN_TESTS
